2bb8ac4648f88ed5b59631c033a1e4b276d5950f
stdj91 <stdj.1907@gmail.com>
2021-09-26 20:46:54 +0200

    experience add

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuOpenPage = document.querySelector('.wrapper');
+const goToAbout = document.getElementById('about-section');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
@@ -12,3 +13,15 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 })
+goToAbout.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpenPage.classList.add('active')
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpenPage.classList.remove('active');
+        menuOpen = false;
+    }
+})
+
