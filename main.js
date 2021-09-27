1a1bd3e0f5ebd1f4877dcd213134fadcc8bcf103
stdj91 <stdj.1907@gmail.com>
2021-09-27 09:31:55 +0200

    add logic for experience section

--- main.js
+++ main.js
@@ -25,3 +25,59 @@ goToAbout.addEventListener('click', () => {
     }
 })
 
+/* ================ Experience Animation ============================*/
+
+const firstCompany = document.getElementById('first-company');
+const secondCompany = document.getElementById('second-company');
+const thirdCompany = document.getElementById('third-company');
+let firComp = true;
+let secComp = false;
+let thiComp = false;
+
+secondCompany.addEventListener('click', () => {
+    if(!secComp && firComp === true && thiComp === false) {
+        firstCompany.classList.remove('job-active');
+        secondCompany.classList.add('job-active');
+        secComp = true;
+        firComp = false;
+        thiComp = false;
+    } else if (!secComp && firComp === false && thiComp === true) {
+        thirdCompany.classList.remove('job-active');
+        secondCompany.classList.add('job-active');
+        secComp = true;
+        firComp = false;
+        thiComp = false;
+    }
+})
+
+thirdCompany.addEventListener('click', () => {
+    if(!thiComp && firComp === true && secComp === false) {
+        firstCompany.classList.remove('job-active');
+        thirdCompany.classList.add('job-active');
+        secComp = false;
+        firComp = false;
+        thiComp = true;
+    } else if (!thiComp && firComp === false && secComp === true) {
+        secondCompany.classList.remove('job-active');
+        thirdCompany.classList.add('job-active');
+        secComp = false;
+        firComp = false;
+        thiComp = true;
+    }
+})
+
+firstCompany.addEventListener('click', () => {
+    if(!firComp && secComp === true && thiComp === false) {
+        secondCompany.classList.remove('job-active');
+        firstCompany.classList.add('job-active');
+        secComp = false;
+        firComp = true;
+        thiComp = false;
+    } else if (!firComp && thiComp === true && secComp === false) {
+        thirdCompany.classList.remove('job-active');
+        firstCompany.classList.add('job-active');
+        secComp = false;
+        firComp = true;
+        thiComp = false;
+    }
+})
