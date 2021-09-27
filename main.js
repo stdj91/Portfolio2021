8c3a8f7887e4e5740fab94b509ff6a16fe4f0e11
stdj91 <stdj.1907@gmail.com>
2021-09-27 12:54:50 +0200

    logic for experience job description toggle

--- main.js
+++ main.js
@@ -30,6 +30,9 @@ goToAbout.addEventListener('click', () => {
 const firstCompany = document.getElementById('first-company');
 const secondCompany = document.getElementById('second-company');
 const thirdCompany = document.getElementById('third-company');
+const descFirstJ = document.getElementById('one');
+const descSecondJ = document.getElementById('two');
+const descThirdJ = document.getElementById('three')
 let firComp = true;
 let secComp = false;
 let thiComp = false;
@@ -38,12 +41,16 @@ secondCompany.addEventListener('click', () => {
     if(!secComp && firComp === true && thiComp === false) {
         firstCompany.classList.remove('job-active');
         secondCompany.classList.add('job-active');
+        descFirstJ.classList.add('show-experience');
+        descSecondJ.classList.remove('show-experience');
         secComp = true;
         firComp = false;
         thiComp = false;
     } else if (!secComp && firComp === false && thiComp === true) {
         thirdCompany.classList.remove('job-active');
         secondCompany.classList.add('job-active');
+        descThirdJ.classList.add('show-experience');
+        descSecondJ.classList.remove('show-experience');
         secComp = true;
         firComp = false;
         thiComp = false;
@@ -54,12 +61,16 @@ thirdCompany.addEventListener('click', () => {
     if(!thiComp && firComp === true && secComp === false) {
         firstCompany.classList.remove('job-active');
         thirdCompany.classList.add('job-active');
+        descFirstJ.classList.add('show-experience');
+        descThirdJ.classList.remove('show-experience');
         secComp = false;
         firComp = false;
         thiComp = true;
     } else if (!thiComp && firComp === false && secComp === true) {
         secondCompany.classList.remove('job-active');
         thirdCompany.classList.add('job-active');
+        descSecondJ.classList.add('show-experience');
+        descThirdJ.classList.remove('show-experience');
         secComp = false;
         firComp = false;
         thiComp = true;
@@ -70,12 +81,16 @@ firstCompany.addEventListener('click', () => {
     if(!firComp && secComp === true && thiComp === false) {
         secondCompany.classList.remove('job-active');
         firstCompany.classList.add('job-active');
+        descSecondJ.classList.add('show-experience');
+        descFirstJ.classList.remove('show-experience');
         secComp = false;
         firComp = true;
         thiComp = false;
     } else if (!firComp && thiComp === true && secComp === false) {
         thirdCompany.classList.remove('job-active');
         firstCompany.classList.add('job-active');
+        descThirdJ.classList.add('show-experience');
+        descFirstJ.classList.remove('show-experience');
         secComp = false;
         firComp = true;
         thiComp = false;
